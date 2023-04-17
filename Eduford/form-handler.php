92c4d486c8a765137ae4ecb42b4b2e2a79e3a626
Abhishek Majumder <108408956+Abhishek6967@users.noreply.github.com>
2023-04-17 23:47:26 +0530

    Add files via upload

---
+++ Eduford/form-handler.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'abirmajumder977gamil.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Message: $subject.\n".
+              "User Message: $message .\n";
+
+$to = 'b200037@nitsikkim.ac';
+
+$headers = "From: $email_from \r\n";
+
+$headers = "Reply-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
